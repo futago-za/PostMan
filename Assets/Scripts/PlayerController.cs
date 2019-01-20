ab4ff1f8c13ecf86e41c73c90de72a988ec704ce
Daiki Naito <daiki0525tymfar@gmail.com>
2019-01-20 21:30:55 +0900

    Add PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,36 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed = 3;
+    public float moveSpeed = 3f;
 
     public float turnSmoothTime = 0.2f;
     public float speedSmoothTime = 0.1f;
 
+    float currentSpeed;
     float turnSmoothVelocity;
     float speedSmoothVelocity;
-    float currentSpeed;
 
     Rigidbody rigidbody;
+    Animator animator;
 
-	void Start () {
+    void Start() {
         rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	void FixedUpdate () {
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate() {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 movementDir = movement.normalized;
-        
-        if(movementDir != Vector3.zero) {
-            float targetRotation = Mathf.Atan2(movementDir.x, movementDir.z) * Mathf.Rad2Deg;
+
+        if (movementDir != Vector3.zero) {
+            float targetRotation = Mathf.Atan2(movementDir.x, movement.z) * Mathf.Rad2Deg;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
 
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, moveSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, moveSpeed * movementDir.magnitude, ref speedSmoothVelocity, speedSmoothTime);
 
         Vector3 velocity = transform.forward * currentSpeed;
 
         rigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
 
         currentSpeed = velocity.magnitude;
-	}
+
+        // モーション
+        float animationSpeedPercent = currentSpeed / moveSpeed;
+        //animator.SetFloat("speedPercent", animationSpeedPercent);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
 }
